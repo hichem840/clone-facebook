c5e6542d3fb90e09af66ea210ffccf8d43455103
hichem <kh_boulouza@esi.dz>
2021-08-11 14:51:26 +0100

    show password

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const input = document.querySelectorAll("input");
-console.log(input)
+//const input = document.querySelectorAll("input");
+///console.log(input)
 
 //input.addEventListener("click",( ) => {
  //   console.log("hichem")
 //}),
+const password = document.getElementById("password");
+const togglePassword = document.getElementById("toggle-password");
+
+
+function toggleClicked() {  
+    if (togglePassword.checked) {
+      password.type = "text";
+      
+    
+    } else {
+      password.type = "password";
+    }
+    password.classList.toggle('visible'); 
+  }
+  
+  togglePassword.addEventListener("click", toggleClicked);
